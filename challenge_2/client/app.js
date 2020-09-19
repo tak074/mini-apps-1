64f8702423abfd594d1742510187b558fc8e64e5
unknown <tak@ucsd.com>
2020-09-18 21:04:56 -0700

    connected client to server

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,12 @@
 
-
+// function handleSubmit() {
+//   let text = document.getElementById('textarea').value;
+//   // debugger;
+//   // $.post('/text',
+//   // {
+//   //   text: text
+//   // },
+//   // function(data){
+//   //   alert("Data: " + data + "\nStatus: " + status);
+//   // })
+// }
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,25 @@
+const express = require('express');
+const App = express();
+const port = 2020;
+const Path = require('path');
+const bodyParser = require('body-parser');
 
+App.use(express.static(Path.join(__dirname,'client')));
+App.use(bodyParser.json());
+App.use(bodyParser.urlencoded({extended: true}));
 
+App.post('/', (req, res) => {
+  console.log(req.body);
+  // req.on('data', (chunk) => {
+  //   console.log(chunk.toString());
+  //   console.log('chunk2');
+  //   console.log(chunk);
+  // });
+
+  res.status(200);
+  res.send('hola');
+})
+
+App.listen(port,() => {
+  console.log(`listening to localhost:${port}`);
+})
